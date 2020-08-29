dded59f2232df382260ae530df9ae5484cde3fc9
aytami <68582141+aytami@users.noreply.github.com>
2020-08-29 12:52:27 +0800

    Add files via upload

--- speedtest.js
+++ speedtest.js
@@ -1,297 +1,183 @@
-function Speedtest() {
-  this._serverList = []; //when using multiple points of test, this is a list of test points
-  this._selectedServer = null; //when using multiple points of test, this is the selected server
-  this._settings = {}; //settings for the speedtest worker
-  this._state = 0; //0=adding settings, 1=adding servers, 2=server selection done, 3=test running, 4=done
-  console.log(
-    "LibreSpeed by Federico Dossena v5.1 - https://github.com/librespeed/speedtest"
-  );
-}
-Speedtest.prototype = {
-  constructor: Speedtest,
-  /**
-   * Returns the state of the test: 0=adding settings, 1=adding servers, 2=server selection done, 3=test running, 4=done
-   */
-  getState: function() {
-    return this._state;
-  },
-  /**
-   * Change one of the test settings from their defaults.
-   * - parameter: string with the name of the parameter that you want to set
-   * - value: new value for the parameter
-   *
-   * Invalid values or nonexistant parameters will be ignored by the speedtest worker.
-   */
-  setParameter: function(parameter, value) {
-    if (this._state != 0)
-      throw "You cannot change the test settings after adding server or starting the test";
-    this._settings[parameter] = value;
-    if(parameter === "temeletry_extra"){
-        this._originalExtra=this._settings.telemetry_extra;
+function I(i){return document.getElementById(i);}
+
+var SPEEDTEST_SERVERS="backend-servers/servers.php"
+
+//INITIALIZE SPEEDTEST
+var s=new Speedtest(); //create speedtest object
+s.setParameter("telemetry_level","basic"); //enable telemetry
+
+//SERVER AUTO SELECTION
+function initServers(){
+    var noServersAvailable=function(){
+        I("message").innerHTML="No servers available";
     }
-  },
-  /**
-   * Used internally to check if a server object contains all the required elements.
-   * Also fixes the server URL if needed.
-   */
-  _checkServerDefinition: function(server) {
-    try {
-      if (typeof server.name !== "string")
-        throw "Name string missing from server definition (name)";
-      if (typeof server.server !== "string")
-        throw "Server address string missing from server definition (server)";
-      if (server.server.charAt(server.server.length - 1) != "/")
-        server.server += "/";
-      if (server.server.indexOf("//") == 0)
-        server.server = location.protocol + server.server;
-      if (typeof server.dlURL !== "string")
-        throw "Download URL string missing from server definition (dlURL)";
-      if (typeof server.ulURL !== "string")
-        throw "Upload URL string missing from server definition (ulURL)";
-      if (typeof server.pingURL !== "string")
-        throw "Ping URL string missing from server definition (pingURL)";
-      if (typeof server.getIpURL !== "string")
-        throw "GetIP URL string missing from server definition (getIpURL)";
-    } catch (e) {
-      throw "Invalid server definition";
-    }
-  },
-  /**
-   * Add a test point (multiple points of test)
-   * server: the server to be added as an object. Must contain the following elements:
-   *  {
-   *       name: "User friendly name",
-   *       server:"http://yourBackend.com/",   URL to your server. You can specify http:// or https://. If your server supports both, just write // without the protocol
-   *       dlURL:"garbage.php"   path to garbage.php or its replacement on the server
-   *       ulURL:"empty.php"   path to empty.php or its replacement on the server
-   *       pingURL:"empty.php"   path to empty.php or its replacement on the server. This is used to ping the server by this selector
-   *       getIpURL:"getIP.php"   path to getIP.php or its replacement on the server
-   *   }
-   */
-  addTestPoint: function(server) {
-    this._checkServerDefinition(server);
-    if (this._state == 0) this._state = 1;
-    if (this._state != 1) throw "You can't add a server after server selection";
-    this._settings.mpot = true;
-    this._serverList.push(server);
-  },
-  /**
-   * Same as addTestPoint, but you can pass an array of servers
-   */
-  addTestPoints: function(list) {
-    for (var i = 0; i < list.length; i++) this.addTestPoint(list[i]);
-  },
-  /**
-   * Returns the selected server (multiple points of test)
-   */
-  getSelectedServer: function() {
-    if (this._state < 2 || this._selectedServer == null)
-      throw "No server is selected";
-    return this._selectedServer;
-  },
-  /**
-   * Manually selects one of the test points (multiple points of test)
-   */
-  setSelectedServer: function(server) {
-    this._checkServerDefinition(server);
-    if (this._state == 3)
-      throw "You can't select a server while the test is running";
-    this._selectedServer = server;
-    this._state = 2;
-  },
-  /**
-   * Automatically selects a server from the list of added test points. The server with the lowest ping will be chosen. (multiple points of test)
-   * The process is asynchronous and the passed result callback function will be called when it's done, then the test can be started.
-   */
-  selectServer: function(result) {
-    if (this._state != 1) {
-      if (this._state == 0) throw "No test points added";
-      if (this._state == 2) throw "Server already selected";
-      if (this._state >= 3)
-        throw "You can't select a server while the test is running";
-    }
-    if (this._selectServerCalled) throw "selectServer already called"; else this._selectServerCalled=true;
-    /*this function goes through a list of servers. For each server, the ping is measured, then the server with the function result is called with the best server, or null if all the servers were down.
-     */
-    var select = function(serverList, result) {
-      //pings the specified URL, then calls the function result. Result will receive a parameter which is either the time it took to ping the URL, or -1 if something went wrong.
-      var PING_TIMEOUT = 2000;
-      var USE_PING_TIMEOUT = true; //will be disabled on unsupported browsers
-      if (/MSIE.(\d+\.\d+)/i.test(navigator.userAgent)) {
-        //IE11 doesn't support XHR timeout
-        USE_PING_TIMEOUT = false;
-      }
-      var ping = function(url, result) {
-        url += (url.match(/\?/) ? "&" : "?") + "cors=true";
-        var xhr = new XMLHttpRequest();
-        var t = new Date().getTime();
-        xhr.onload = function() {
-          if (xhr.responseText.length == 0) {
-            //we expect an empty response
-            var instspd = new Date().getTime() - t; //rough timing estimate
-            try {
-              //try to get more accurate timing using performance API
-              var p = performance.getEntriesByName(url);
-              p = p[p.length - 1];
-              var d = p.responseStart - p.requestStart;
-              if (d <= 0) d = p.duration;
-              if (d > 0 && d < instspd) instspd = d;
-            } catch (e) {}
-            result(instspd);
-          } else result(-1);
-        }.bind(this);
-        xhr.onerror = function() {
-          result(-1);
-        }.bind(this);
-        xhr.open("GET", url);
-        if (USE_PING_TIMEOUT) {
-          try {
-            xhr.timeout = PING_TIMEOUT;
-            xhr.ontimeout = xhr.onerror;
-          } catch (e) {}
-        }
-        xhr.send();
-      }.bind(this);
-      //this function repeatedly pings a server to get a good estimate of the ping. When it's done, it calls the done function without parameters. At the end of the execution, the server will have a new parameter called pingT, which is either the best ping we got from the server or -1 if something went wrong.
-      var PINGS = 3, //up to 3 pings are performed, unless the server is down...
-        SLOW_THRESHOLD = 500; //...or one of the pings is above this threshold
-      var checkServer = function(server, done) {
-        var i = 0;
-        server.pingT = -1;
-        if (server.server.indexOf(location.protocol) == -1) done();
-        else {
-          var nextPing = function() {
-            if (i++ == PINGS) {
-              done();
-              return;
+    var runServerSelect=function(){
+        s.selectServer(function(server){
+            if(server!=null){ //at least 1 server is available
+                I("loading").className="hidden"; //hide loading message
+                //populate server list for manual selection
+                for(var i=0;i<SPEEDTEST_SERVERS.length;i++){
+                    if(SPEEDTEST_SERVERS[i].pingT==-1) continue;
+                    var option=document.createElement("option");
+                    option.value=i;
+                    option.textContent=SPEEDTEST_SERVERS[i].name;
+                    if(SPEEDTEST_SERVERS[i]===server) option.selected=true;
+                    I("server").appendChild(option);
+                }
+                //show test UI
+                I("testWrapper").className="visible";
+                updateSponsor();
+                initUI();
+            }else{ //no servers are available, the test cannot proceed
+                noServersAvailable();
             }
-            ping(
-              server.server + server.pingURL,
-              function(t) {
-                if (t >= 0) {
-                  if (t < server.pingT || server.pingT == -1) server.pingT = t;
-                  if (t < SLOW_THRESHOLD) nextPing();
-                  else done();
-                } else done();
-              }.bind(this)
-            );
-          }.bind(this);
-          nextPing();
-        }
-      }.bind(this);
-      //check servers in list, one by one
-      var i = 0;
-      var done = function() {
-        var bestServer = null;
-        for (var i = 0; i < serverList.length; i++) {
-          if (
-            serverList[i].pingT != -1 &&
-            (bestServer == null || serverList[i].pingT < bestServer.pingT)
-          )
-            bestServer = serverList[i];
-        }
-        result(bestServer);
-      }.bind(this);
-      var nextServer = function() {
-        if (i == serverList.length) {
-          done();
-          return;
-        }
-        checkServer(serverList[i++], nextServer);
-      }.bind(this);
-      nextServer();
-    }.bind(this);
-    //parallel server selection
-    var CONCURRENCY = 6;
-    var serverLists = [];
-    for (var i = 0; i < CONCURRENCY; i++) {
-      serverLists[i] = [];
-    }
-    for (var i = 0; i < this._serverList.length; i++) {
-      serverLists[i % CONCURRENCY].push(this._serverList[i]);
-    }
-    var completed = 0;
-    var bestServer = null;
-    for (var i = 0; i < CONCURRENCY; i++) {
-      select(
-        serverLists[i],
-        function(server) {
-          if (server != null) {
-            if (bestServer == null || server.pingT < bestServer.pingT)
-              bestServer = server;
-          }
-          completed++;
-          if (completed == CONCURRENCY) {
-            this._selectedServer = bestServer;
-            this._state = 2;
-            if (result) result(bestServer);
-          }
-        }.bind(this)
-      );
-    }
-  },
-  /**
-   * Starts the test.
-   * During the test, the onupdate(data) callback function will be called periodically with data from the worker.
-   * At the end of the test, the onend(aborted) function will be called with a boolean telling you if the test was aborted or if it ended normally.
-   */
-  start: function() {
-    if (this._state == 3) throw "Test already running";
-    this.worker = new Worker("speedtest_worker.js?r=" + Math.random());
-    this.worker.onmessage = function(e) {
-      if (e.data === this._prevData) return;
-      else this._prevData = e.data;
-      var data = JSON.parse(e.data);
-      try {
-        if (this.onupdate) this.onupdate(data);
-      } catch (e) {
-        console.error("Speedtest onupdate event threw exception: " + e);
-      }
-      if (data.testState >= 4) {
-        try {
-          if (this.onend) this.onend(data.testState == 5);
-        } catch (e) {
-          console.error("Speedtest onend event threw exception: " + e);
-        }
-        clearInterval(this.updater);
-        this._state = 4;
-      }
-    }.bind(this);
-    this.updater = setInterval(
-      function() {
-        this.worker.postMessage("status");
-      }.bind(this),
-      200
-    );
-    if (this._state == 1)
-        throw "When using multiple points of test, you must call selectServer before starting the test";
-    if (this._state == 2) {
-      this._settings.url_dl =
-        this._selectedServer.server + this._selectedServer.dlURL;
-      this._settings.url_ul =
-        this._selectedServer.server + this._selectedServer.ulURL;
-      this._settings.url_ping =
-        this._selectedServer.server + this._selectedServer.pingURL;
-      this._settings.url_getIp =
-        this._selectedServer.server + this._selectedServer.getIpURL;
-      if (typeof this._originalExtra !== "undefined") {
-        this._settings.telemetry_extra = JSON.stringify({
-          server: this._selectedServer.name,
-          extra: this._originalExtra
-        });
-      } else
-        this._settings.telemetry_extra = JSON.stringify({
-          server: this._selectedServer.name
         });
     }
-    this._state = 3;
-    this.worker.postMessage("start " + JSON.stringify(this._settings));
-  },
-  /**
-   * Aborts the test while it's running.
-   */
-  abort: function() {
-    if (this._state < 3) throw "You cannot abort a test that's not started yet";
-    if (this._state < 4) this.worker.postMessage("abort");
-  }
-};
+    if(typeof SPEEDTEST_SERVERS === "string"){
+        //need to fetch list of servers from specified URL
+        s.loadServerList(SPEEDTEST_SERVERS,function(servers){
+            if(servers==null){ //failed to load server list
+                noServersAvailable();
+            }else{ //server list loaded
+                SPEEDTEST_SERVERS=servers;
+                runServerSelect();
+            }
+        });
+    }else{
+        //hardcoded server list
+        s.addTestPoints(SPEEDTEST_SERVERS);
+        runServerSelect();
+    }
+}
+
+var meterBk=/Trident.*rv:(\d+\.\d+)/i.test(navigator.userAgent)?"#EAEAEA":"#80808040";
+var dlColor="#6060AA",
+	ulColor="#616161";
+var progColor=meterBk;
+
+//CODE FOR GAUGES
+function drawMeter(c,amount,bk,fg,progress,prog){
+	var ctx=c.getContext("2d");
+	var dp=window.devicePixelRatio||1;
+	var cw=c.clientWidth*dp, ch=c.clientHeight*dp;
+	var sizScale=ch*0.0055;
+	if(c.width==cw&&c.height==ch){
+		ctx.clearRect(0,0,cw,ch);
+	}else{
+		c.width=cw;
+		c.height=ch;
+	}
+	ctx.beginPath();
+	ctx.strokeStyle=bk;
+	ctx.lineWidth=12*sizScale;
+	ctx.arc(c.width/2,c.height-58*sizScale,c.height/1.8-ctx.lineWidth,-Math.PI*1.1,Math.PI*0.1);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.strokeStyle=fg;
+	ctx.lineWidth=12*sizScale;
+	ctx.arc(c.width/2,c.height-58*sizScale,c.height/1.8-ctx.lineWidth,-Math.PI*1.1,amount*Math.PI*1.2-Math.PI*1.1);
+	ctx.stroke();
+	if(typeof progress !== "undefined"){
+		ctx.fillStyle=prog;
+		ctx.fillRect(c.width*0.3,c.height-16*sizScale,c.width*0.4*progress,4*sizScale);
+	}
+}
+function mbpsToAmount(s){
+	return 1-(1/(Math.pow(1.3,Math.sqrt(s))));
+}
+function format(d){
+    d=Number(d);
+    if(d<10) return d.toFixed(2);
+    if(d<100) return d.toFixed(1);
+    return d.toFixed(0);
+}
+
+//UI CODE
+var uiData=null;
+function startStop(){
+    if(s.getState()==3){
+		//speedtest is running, abort
+		s.abort();
+		data=null;
+		I("startStopBtn").className="";
+		I("server").disabled=false;
+		initUI();
+	}else{
+		//test is not running, begin
+		I("startStopBtn").className="running";
+		I("shareArea").style.display="none";
+		I("server").disabled=true;
+		s.onupdate=function(data){
+            uiData=data;
+		};
+		s.onend=function(aborted){
+            I("startStopBtn").className="";
+            I("server").disabled=false;
+            updateUI(true);
+            if(!aborted){
+                //if testId is present, show sharing panel, otherwise do nothing
+                try{
+                    var testId=uiData.testId;
+                    if(testId!=null){
+                        var shareURL=window.location.href.substring(0,window.location.href.lastIndexOf("/"))+"/results/?id="+testId;
+                        I("resultsImg").src=shareURL;
+                        I("resultsURL").value=shareURL;
+                        I("testId").innerHTML=testId;
+                        I("shareArea").style.display="";
+                    }
+                }catch(e){}
+            }
+		};
+		s.start();
+	}
+}
+//this function reads the data sent back by the test and updates the UI
+function updateUI(forced){
+	if(!forced&&s.getState()!=3) return;
+	if(uiData==null) return;
+	var status=uiData.testState;
+	I("ip").textContent=uiData.clientIp;
+	I("dlText").textContent=(status==1&&uiData.dlStatus==0)?"...":format(uiData.dlStatus);
+	drawMeter(I("dlMeter"),mbpsToAmount(Number(uiData.dlStatus*(status==1?oscillate():1))),meterBk,dlColor,Number(uiData.dlProgress),progColor);
+	I("ulText").textContent=(status==3&&uiData.ulStatus==0)?"...":format(uiData.ulStatus);
+	drawMeter(I("ulMeter"),mbpsToAmount(Number(uiData.ulStatus*(status==3?oscillate():1))),meterBk,ulColor,Number(uiData.ulProgress),progColor);
+	I("pingText").textContent=format(uiData.pingStatus);
+	I("jitText").textContent=format(uiData.jitterStatus);
+}
+function oscillate(){
+	return 1+0.02*Math.sin(Date.now()/100);
+}
+//update the UI every frame
+window.requestAnimationFrame=window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.msRequestAnimationFrame||(function(callback,element){setTimeout(callback,1000/60);});
+function frame(){
+	requestAnimationFrame(frame);
+	updateUI();
+}
+frame(); //start frame loop
+//function to (re)initialize UI
+function initUI(){
+	drawMeter(I("dlMeter"),0,meterBk,dlColor,0);
+	drawMeter(I("ulMeter"),0,meterBk,ulColor,0);
+	I("dlText").textContent="";
+	I("ulText").textContent="";
+	I("pingText").textContent="";
+	I("jitText").textContent="";
+	I("ip").textContent="";
+}
+function updateSponsor(){
+    var server=SPEEDTEST_SERVERS[I("server").value];
+    if(server==null) return;
+    if(server.sponsorName!=null){
+        I("sponsorArea").style.visibility="visible";
+        if(server.sponsorURL!=null){
+                var a=document.createElement("a");
+                a.href=server.sponsorURL;
+                a.innerText=server.sponsorName;
+                a.target="_blank";
+                I("sponsor").innerHTML="";
+                I("sponsor").appendChild(a);
+        }else{
+            I("sponsor").innerText=server.sponsorName
+        }
+    }else I("sponsorArea").style.visibility="hidden";
+}
